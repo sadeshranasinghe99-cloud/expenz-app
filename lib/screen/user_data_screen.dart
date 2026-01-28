00440b923eb9e39350451b98177a0f9f91e149df
Sadesh Ranasinghe <sadeshranasinghe99@gmail.com>
2026-01-28 20:03:22 +0530

    Validation

--- lib/screen/user_data_screen.dart
+++ lib/screen/user_data_screen.dart
@@ -1,3 +1,6 @@
+import 'package:expenz/constants/colors.dart';
+import 'package:expenz/constants/const.dart';
+import 'package:expenz/widget/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class UserDataScreen extends StatefulWidget {
@@ -8,10 +11,189 @@ class UserDataScreen extends StatefulWidget {
 }
 
 class _UserDataScreenState extends State<UserDataScreen> {
+  //for The check box
+  bool _rememberMe = false;
+  //For Validation
+  final _formKey = GlobalKey<FormState>();
+
+  //Controller
+
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("user", style: TextStyle(fontSize: 30),)),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(defaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Enter your\n Personal Details",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                ),
+
+                SizedBox(height: 30),
+
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _userNameController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter Name";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 25),
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter Email";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 25),
+                      TextFormField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter Password";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 25),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter Confirm Password";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Confirm Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 25),
+
+                      Row(
+                        children: [
+                          Text(
+                            "Remember Me for the next time",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: kGrey,
+                            ),
+                          ),
+
+                          Expanded(
+                            child: CheckboxListTile(
+                              activeColor: kMainColor,
+                              value: _rememberMe,
+                              onChanged: (value) {
+                                setState(() {
+                                  _rememberMe = value!;
+                                  print(_rememberMe);
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(height: 20),
+
+                      Container(
+                        child: GestureDetector(
+                          onTap: () {
+                            if (_formKey.currentState!.validate()) {
+                              String userName = _userNameController.text;
+                              String email = _emailController.text;
+                              String Password = _passwordController.text;
+                              String confirmPassword =
+                                  _confirmPasswordController.text;
+
+                              print(
+                                "$userName $email $Password $confirmPassword",
+                              );
+                            }
+                          },
+                          child: CustomButton(
+                            buttonColor: kMainColor,
+                            buttonName: "Next",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
